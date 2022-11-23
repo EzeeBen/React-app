346e1aac55b8da381b409a6c1d59670fccd3cc45
EzeeBen <ezee.ds94@gmail.com>
2022-11-23 18:18:55 -0300

    Primera entrega

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ItemListContainer from "./ItemListContainer";
+import Navbar from "./navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  return(
+    <>
+    <Navbar />
+    <ItemListContainer mensaje = "Buenas noches tutor! Corrijame"/>
+    </>
+    
+  )
 }
 
 export default App;
---
+++ src/navbar.jsx
@@ -0,0 +1,27 @@
+const Navbar = () => {
+    return(
+    <nav className="nav">
+        <ul class="nav__lista">
+                <li className="nav__item ">
+                    <p>Mi Shop | Pasillo 4</p>
+                </li>
+                <li className="nav__item ">
+                    <p>Inicio</p>
+                </li>
+                <li className="nav__item ">
+                    <p>Bebidas</p>
+                </li>
+                <li className="nav__item ">
+                    <p>Comida</p>
+                </li>
+                <li className="nav__item ">
+                    <p>Nuestros locales</p>
+                </li>
+                <li className="nav__item ">
+                    <p>Contactanos</p>
+                </li>
+            </ul>
+    </nav>)
+}
+
+export default Navbar;
